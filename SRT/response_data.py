fa2348f1c8b89cd27db54e19aea9d26eb78f182e
kjm0321 <kjm0321@midasit.com>
2024-12-23 14:08:01 +0900

    올바른 경로를 이용하세요 bug fix

--- SRT/response_data.py
+++ SRT/response_data.py
@@ -48,6 +48,8 @@ class SRTResponseData:
 
     def message(self):
         return self._status.get("msgTxt", "")
+    def message_code(self):
+        return self._status.get("msgCd", "")
 
     # get parse result
     def get_all(self):
